49d0150c51a7a8dbe93155450079bba37477fee6
Khang Nguyễn <khangnguyenhoangbao@gmail.com>
2025-03-13 00:15:11 +0700

    config app and server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
-import express from "express";
-import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import app from "./app.js";
 
-const app = express();
-const port = 3000;
+dotenv.config();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Server is running at port: ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running at port: ${PORT}`);
 });

7280d7bc0636bf1b88bd413dee957bb23cf9deff
Khang Nguyễn <khangnguyenhoangbao@gmail.com>
2025-03-12 23:25:34 +0700

    config json

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import express from "express";
+import bodyParser from "body-parser";
+
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use("/api/login", loginRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running at port: ${port}`);

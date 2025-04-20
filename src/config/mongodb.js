8c9f9bbd384ad1e85decafeca44813cb249b77f9
Khang Nguyễn <khangnguyenhoangbao@gmail.com>
2025-04-20 15:54:54 +0700

    connect mongoDB and env setting

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,30 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import { env } from '~/config/environment'
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+let shoesDatabaseInstance = null
+
+//Client to connect MongoDB
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+//Database connection
+export const CONNECT_DB= async () => {
+  //Connect to MongoDB Atlas with URI
+  await mongoClientInstance.connect()
+
+  shoesDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
+}
+
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close()
+}
+
+export const GET_DB = () => {
+  if (!shoesDatabaseInstance) throw new Error('Must connect Database first!')
+  return shoesDatabaseInstance
+}

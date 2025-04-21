fa4ebe8755538bf6fb9e4bdd0f28c8469fb9352f
Khang Nguyễn <khangnguyenhoangbao@gmail.com>
2025-04-21 14:06:36 +0700

    Add Error Handling

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,13 +2,13 @@ import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (req, res, next) => {
   try {
-    console.log('req.body:', req.body)
+    // console.log('req.body:', req.body)
+    // console.log('req.query:', req.query)
+    // console.log('req.params:', req.params)
+
+    // throw new ApiError(StatusCodes.BAD_GATEWAY, 'Test error')
     res.status(StatusCodes.CREATED).json({ message: 'POST from Controller: APIs create list products.' })
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })
-  }
+  } catch (error) { next(error) }
 }
 
 export const productController = {
